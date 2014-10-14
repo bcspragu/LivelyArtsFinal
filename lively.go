4f3968d6c3e67e20a1e1ee22b29ade56a5015301
Brandon Sprague <brandon00sprague@gmail.com>
2014-10-13 21:50:22 -0400

    Starting it up

---
+++ lively.go
@@ -0,0 +1,49 @@
+package lively
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"html/template"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var templates = template.Must(template.ParseGlob("templates/*"))
+var store = sessions.NewCookieStore([]byte("thisistotallyapasswordimcommittingtogithub"))
+var userCount = 0
+var userLock = &sync.Mutex{}
+
+func init() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", mainHandler).Methods("POST")
+	r.HandleFunc("/", voteHandler).Methods("GET")
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./js"))))
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
+	http.Handle("/", r)
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	session, err := store.Get(r, "info")
+	if err != nil {
+		log.Print("Error with getting session:", err)
+	}
+	if session.IsNew {
+		userLock.Lock()
+		userCount++
+		session.Values["ID"] = userCount
+		session.Values["Vote"] = nil
+		userLock.Unlock()
+	}
+	session.Save(r, w)
+	templates.ExecuteTemplate(w, "lively.html", struct{}{})
+}
+
+func voteHandler(w http.ResponseWriter, r *http.Request) {
+	session, err := store.Get(r, "info")
+	if err != nil {
+		log.Print("Error with getting session:", err)
+	}
+	session.Values["Vote"] = r.FormValue("Vote")
+	session.Save(r, w)
+}
