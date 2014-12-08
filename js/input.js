6507a12dcb3d5ac8ac41dd4423e1e2a33b9e8e18
Brandon Sprague <brandon00sprague@gmail.com>
2014-12-08 03:08:43 -0500

    More mobile friendly

--- js/input.js
+++ js/input.js
@@ -4,6 +4,7 @@ $(function() {
     if (text != "") {
       text = text.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
       text = text.replace(/\s{2,}/g," ");
+      text = text.toLowerCase();
       $.post("/input", {words: text});
     }
   });
